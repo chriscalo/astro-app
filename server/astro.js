a34d1fc513a0ed948fc3a225fdf0078ce747dc51
Chris Calo <chris.calo@gmail.com>
2024-02-27 23:15:41 -0500

    move Astro build into the astro.js server logic

--- server/astro.js
+++ server/astro.js
@@ -1,11 +1,42 @@
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import express from "express";
-import { handler as astroSsrHandler } from "~/dist/server/entry.mjs";
+import { build } from "astro";
 
 const server = express();
 export default server;
 
+const astroReady = new Promise(async function (resolve, reject) {
+  const DEVELOPMENT = (process.env.NODE_ENV !== "production");
+  
+  if (DEVELOPMENT) {
+    await build({
+      root: resolveRelativePath("../"),
+    });
+    resolve(true);
+  } else {
+    resolve(true);
+  }
+});
+
+server.use(async function (req, res, next) {
+  await astroReady;
+  next();
+});
+
 server.use(express.static("dist/client/"));
-server.use(function (req, res, next) {
+
+let astroSsrHandler = null;
+server.use(async function (req, res, next) {
+  if (!astroSsrHandler) {
+    const { handler } = await import("~/dist/server/entry.mjs");
+    astroSsrHandler = handler
+  }
   const { locals } = res;
   astroSsrHandler(req, res, next, locals);
 });
+
+function resolveRelativePath(relativePath) {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  return resolve(__dirname, relativePath);
+}
